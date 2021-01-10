70636a6b30d41f1c307519d3d77fdee66d19ab66
warihsuryono <warih_suryono@yahoo.com>
2021-01-10 14:26:39 +0700

    aaa

---
+++ alter_db/warih.sql
@@ -0,0 +1,36 @@
+customers
+tire_positions
+    id
+    name
+
+tire_types
+    id
+    name
+
+service_types
+    id
+    name
+    
+tire_brands
+    id
+    name
+
+vehicle_brands
+    id
+    name
+
+vehicle_types
+    id
+    name
+
+tires
+    id
+    qrcode
+    serialno
+    brand_id
+    type_id
+
+vehicles
+    id
+    nopol
+

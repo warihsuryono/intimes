4f1b698666c4d8e6a36a7f85fa87810a02118652
warih suryono <warih_suryono@yahoo.com>
2021-01-20 16:03:21 +0700

    aaa

--- app/Controllers/Qr_reader.php
+++ app/Controllers/Qr_reader.php
@@ -7,6 +7,8 @@ class Qr_reader extends BaseController
     public function __construct()
     {
         parent::__construct();
+        $this->route_name = "qrcode";
+        $this->menu_ids = $this->get_menu_ids($this->route_name);
     }
 
     public function index()
@@ -14,8 +16,15 @@ class Qr_reader extends BaseController
         echo view('v_qrcode_reader');
     }
 
-    public function qrcode_menu($qrCodeMessage)
+    public function qrcode_menu($qrcode)
     {
-        echo $qrCodeMessage;
+        $this->privilege_check($this->menu_ids);
+        $data["__modulename"] = "QR Code Reader";
+        $data["qrcode"] = $qrcode;
+        $data = $data + $this->common();
+        echo view('v_header', $data);
+        echo view('v_menu');
+        echo view('qr_readers/v_menu');
+        echo view('v_footer');
     }
 }
---
+++ app/Views/qr_readers/v_menu.php
@@ -0,0 +1,18 @@
+<!-- Main content -->
+<div class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-4">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

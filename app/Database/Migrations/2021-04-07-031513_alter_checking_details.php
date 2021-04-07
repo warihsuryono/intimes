214fe564b1142dd4b59e1f4009917971d5971c0d
warih suryono <warih_suryono@yahoo.com>
2021-04-07 13:09:52 +0700

    aaa

--- app/Database/Migrations/2021-04-07-031513_alter_checking_details.php
+++ app/Database/Migrations/2021-04-07-031513_alter_checking_details.php
@@ -4,7 +4,7 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class AlterCheckingDetails extends Migration
+class AlterCheckingDetails031513 extends Migration
 {
 	public function up()
 	{
